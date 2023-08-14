9b468540a88140e8c929dc484679dd8708e3205c
sahilpedamkar21 <sahilpedamkar18@gmail.com>
2023-08-14 17:56:18 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,16 +1,18 @@
 const {City} = require('../models/index');
 
 class CityRepository{
-    async createCity({name}){
+    // creating city database
+    async createCity({name}){   //{name} helps in direct access rather than obj.name
         try{
             const city = await City.create({name});
             return city;
         } catch(error){
+            console.log("Something wrong in createcity function")
             throw {error};
         }
     }
 
-
+ // delete a particular city from database
     async deleteCity({cityId}){
         try{
             await City.destroy({
@@ -19,11 +21,39 @@ class CityRepository{
                 }
             });
         } catch(error){
+            console.log("something wrong in deletecity")
             throw {error};
 
         }
     }
 
+
+    async updateCity(cityId, data){
+        try{
+            const city = await City.update(data, {
+                where:{
+                    id : cityId
+                }
+            });
+            return city;
+
+        } catch(error){
+            console.log("something wrong error in updatecity")
+            throw {error};
+        }
+
+    }
+
+
+    async getCity(cityId){
+        try{
+            const city = await City.findByPk(cityId);
+            return city;
+        }   catch(error){
+                console.log("somethoung wrong in getcity");
+                throw {error};
+        }
+    }
 
 }
 
